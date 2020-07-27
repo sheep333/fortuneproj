c4a4ce321bd954f71197bbe16ad65e1e35b95d6a
sheep333 <yukiko.elike1.51@gmail.com>
2020-07-28 00:26:52 +0900

    add admin models in fortune app

--- fortuneproj/fortune/admin.py
+++ fortuneproj/fortune/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from .models import UserFortuneType, FortuneBoard
 
 # Register your models here.
-admin.register(UserFortuneType)
+admin.site.register(UserFortuneType)
+admin.site.register(FortuneBoard)
